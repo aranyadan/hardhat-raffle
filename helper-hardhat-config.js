112f013fbc439bf95884f7b2ca70e9c1eee00a25
Aranya Dan <aranya.dan1996@gmail.com>
2022-06-22 03:08:13 +0200

    Added raffle contract, and some unit tests

---
+++ helper-hardhat-config.js
@@ -0,0 +1,39 @@
+const { ethers } = require("hardhat")
+
+const networkConfig = {
+    4: {
+        name: "Rinkeby",
+        VRFCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane:
+            "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        subscriptionId: "0",
+        callbackGasLimit: "500000",
+        interval: "300",
+    },
+    80001: {
+        name: "Polygon Mumbai",
+        VRFCoordinatorV2: "0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane:
+            "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f",
+        subscriptionId: "0",
+        callbackGasLimit: "500000",
+        interval: "300",
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("1"),
+        gasLane:
+            "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        callbackGasLimit: "500000",
+        interval: "300",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
---
+++ test/unit/Raffle.test.js
@@ -0,0 +1,71 @@
+const { assert, expect } = require("chai")
+const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
+const {
+    developmentChains,
+    networkConfig,
+} = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle", async function () {
+          let raffle, vrfCoordinatorV2Mock
+          let deployer, raffleEntranceFee, interval
+          const chainId = network.config.chainId
+
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              await deployments.fixture(["all"])
+              raffle = await ethers.getContract("Raffle", deployer)
+              vrfCoordinatorV2Mock = await ethers.getContract(
+                  "VRFCoordinatorV2Mock",
+                  deployer
+              )
+              raffleEntranceFee = await raffle.getEntranceFee()
+              interval = await raffle.getInterval()
+          })
+
+          describe("Constructor", async function () {
+              it("Initializes the raffle correctly", async function () {
+                  // Ideally, 1 assert per it
+                  const raffleState = await raffle.getRaffleState()
+
+                  assert.equal(raffleState.toString(), "0")
+                  assert.equal(
+                      interval.toString(),
+                      networkConfig[chainId]["interval"]
+                  )
+              })
+          })
+
+          describe("Enter Raffle", async function () {
+              it("Reverts when you dont pay enough", async function () {
+                  await expect(raffle.enterRaffle()).to.be.revertedWith(
+                      "Raffle__NotEnoughEthEntered"
+                  )
+              })
+
+              it("Records players when they enter", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  const playerFromContract = await raffle.getPlayer(0)
+                  assert.equal(playerFromContract, deployer)
+              })
+
+              it("Emits event on enter", async function () {
+                  await expect(
+                      raffle.enterRaffle({ value: raffleEntranceFee })
+                  ).to.emit(raffle, "RaffleEnter")
+              })
+
+              it("Doesnt allow entrance when raffle is calculating", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [
+                      interval.toNumber() + 1,
+                  ])
+                  await network.provider.send("evm_mine", [])
+                  await raffle.performUpkeep([])
+                  await expect(
+                      raffle.enterRaffle({ value: raffleEntranceFee })
+                  ).to.be.revertedWith("Raffle__NotOpen")
+              })
+          })
+      })
